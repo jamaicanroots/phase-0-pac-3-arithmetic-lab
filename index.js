4db938cee9bb268041ea972e216df436aa00111c
matthewelam <ematew418@gmail.com>
2022-06-29 08:29:17 -0400

    commit tree 36f17426387eff7b5ecaf92f9577d645fb00c526

---
+++ index.js
@@ -0,0 +1,53 @@
+function add() {
+    // we'll fill this in shortly
+  }
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add() {
+  return "something";
+}
+function add(a, b) {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract(a,b) {
+    return a - b;
+  }
+  function multiply(a,b) {
+    return a * b;
+  }
+  function divide(a,b) {
+    return a / b;
+  }
+  
+  
+
+
+function divideBy3() {
+  return (number /= 3);
+}
+
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
+
+function makeInt(n) {
+    return parseInt (n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
